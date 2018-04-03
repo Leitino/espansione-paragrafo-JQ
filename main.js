729853b28af05604b125f7177d27200d4e400a08
Leitino <alessandro.leita89@gmail.com>
2018-04-03 18:30:30 +0200

    modifiche

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var isExpanded =false
 $('#button').click(function(){
   if (isExpanded) {
-    $('.film_trama, .trama').fadeOut(2000)
+    $('.film_trama, .trama').fadeOut(1000)
     $('#button').html('+')
     isExpanded = false
   }
   else {
-    $('.film_trama, .trama').show(2000);
+    $('.film_trama, .trama').fadeIn(2000);
     $('#button').html('-')
     isExpanded = true
   }
