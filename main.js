ca656b3572f6d4695cdbd53086541856fd3763fb
Leitino <alessandro.leita89@gmail.com>
2018-04-04 10:20:51 +0200

    miglioramento

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
+$(document).ready(function(){
+
 var isExpanded =false
 $('#button').click(function(){
   if (isExpanded) {
-    $('.film_trama, .trama').fadeOut(1000)
+    $('.film_trama, .trama').fadeOut(1000, function(){
     $('#button').html('+')
     isExpanded = false
+   })
   }
   else {
-    $('.film_trama, .trama').fadeIn(2000);
+    $('.film_trama, .trama').fadeIn(2000,function(){
     $('#button').html('-')
     isExpanded = true
+   });
   }
+})
 })
